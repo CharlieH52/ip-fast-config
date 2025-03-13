94ea3d248981cb187728418cf6ee08efbfc9df80
CharlieH52 <carlos.gomez.h52@gmail.com>
2025-03-13 14:44:39 -0600

    [RESTRUCTURE] Deleted the CLI system

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Configuracion-IP_BASE
-Script para la configuración automatizada de la dirección IP del adaptador de red.
+# IP-Fast-Config
+## # Description
+If you have a standard configuration in your organization network, this script can help you for type only the variable inputs of your network configuration.
+
+You can change the dictionary and the variables of the commands depending of your network standard.
+
+## # Dependences
+
+## # Configuration
+```
+- 'NAME':   'Ethernet',     // Name of your network adapter.
+- 'IP'  :   '0.0.0.0',      // Computer IP address.
+- 'MASK':   '0.0.0.0',      // Subnet mask.
+- 'GATE':   '0.0.0.0',      // Gateway address.
+- 'DNS1':   '0.0.0.0',      // First DNS address.
+- 'DNS2':   '0.0.0.0'       // Secondary DNS address.
+```
+
+> [!IMPORTANT]
+>
+> For the most basic function clean the last number of ***IP***.
+> 
+> **EXAMPLE**
+> If your organization only changes the last numbers of the ***IP ADDRESS***
+>*'192.168.0.XXX'* clear the last number in the dictionary just like this:
+>
+> - 'IP': *'192.168.0.'*
+>
+> Check the ***INTERFACE NAME*** of your systems this is the most important parameter for the script and the default is ***Ethernet***.
--- main.py
+++ main.py
@@ -1,102 +1,36 @@
-from time import sleep
 from subprocess import run
-import os
-
-class IPAutomatron:
-    def __init__(self):
-        self.file_path = os.path.join(os.getcwd(), 'base.config')
-        self.dictionary = self.check_base_config()
-
-    def check_base_config(self):
-        dictionary = {}
-        try:
-            with open(self.file_path, 'r') as file:
-                for items in file.readlines():
-                    key, name = items.strip().split('= ')
-                    dictionary[key] = name
-                return dictionary
-            
-        except FileNotFoundError as e:
-            print("Ocurrio un problema al leer el archivo '.config'")
-        
-    def command_execute(self, command_sequence):
-        return run(command_sequence)
-
-    def normal_config_ip(self, input):
-        command = ['netsh', 'interface', 'ipv4', 'set', 'address', f'{self.dictionary['NAME']}', 'static', f'{self.dictionary['IP']}{input}', f'{self.dictionary['MASK']}', f'{self.dictionary['GATE']}', 'store=persistent']
-        print("Estableciendo configuracion...")
-        print(f"Direccion IP: {self.dictionary['IP']}")
-        print(f"Mascara de subred: {self.dictionary['MASK']}")
-        print(f"Puerta de enlace predeterminada: {self.dictionary['IP']}")
-        self.command_execute(command)
-
-    def new_config(self, input):
-        command = ['netsh', 'interface', 'ipv4', 'set', 'address', f'{self.dictionary['NAME']}', 'static', f'{self.dictionary['IP']}{input}', 'store=persistent']
-        self.command_execute(command)
-
-    def set_dns(self):
-        command_DNS1 = ['netsh', 'interface', 'ipv4', 'set', 'dnsservers', f'{self.dictionary['NAME']}', 'static', f'{self.dictionary['DNS1']}', 'primary']
-        print(f"Servidor DNS1: {self.dictionary['DNS1']}")
-        self.command_execute(command_DNS1)
-    
-    def add_dns(self):
-        command_DNS2 = ['netsh', 'interface', 'ipv4', 'add', 'dnsservers', f'{self.dictionary['NAME']}', f'{self.dictionary['DNS2']}']
-        print(f"Servidor DNS2: {self.dictionary['DNS2']}")
-        self.command_execute(command_DNS2)
-    
-    def first_configuration(self, input):
-        self.normal_config_ip(ip)
-        sleep(1)
-        self.set_dns()
-        sleep(1)
-        self.add_dns()
-        sleep(1)
+base_config = {
+    'NAME': 'Ethernet',
+    'IP': '192.168.0.',
+    'MASK': '255.255.255.0',
+    'GATE': '192.168.0.254',
+    'DNS1': '192.168.0.1',
+    'DNS2': '192.168.1.254'
+}
 
 if __name__ == '__main__':
-    ipa = IPAutomatron()
+    while True:
+        addr = input()
 
-    PRODUCTION_MODE = True
-    if PRODUCTION_MODE == True:
-        while True:
-            try:
-                if not os.path.isfile(ipa.file_path):
-                    with open(ipa.file_path, 'w') as file:
-                        file.write(
-                            'NAME= \n'
-                            'IP= \n'
-                            'MASK= \n'  
-                            'GATE= \n' 
-                            'DNS1= \n'
-                            'DNS2= '
-                        )
-                    print(f"Se ha creado el archivo con exito el archivo '{ipa.file_path}'.")
-                    print("Agrega los valores base de tu configuracion.")
-                    print("No olvides dejar vacio el ultimo campo de la direccion IP.")
-                    print("Ejemplo: 192.168.0. ")
-                    break
+        if addr.isnumeric():
+            break
+        
+        print(f'{addr} el valor ingresado es incorrecto...' )
+        input()
 
-                if os.path.isfile(ipa.file_path):
-                    break
+    # def normal_base_config_ip(addr):
+    #     command = ['netsh', 'interface', 'ipv4', 'set', 'address', f'{base_config['NAME']}', 'static', f'{base_config['IP']}{addr}', f'{base_config['MASK']}', f'{base_config['GATE']}', 'store=persistent']
+    #     print(f"Direccion IP: {base_config['IP']}")
+    #     print(f"Mascara de subred: {base_config['MASK']}")
+    #     print(f"Puerta de enlace predeterminada: {base_config['IP']}")
+    #     run(command)
 
-            except PermissionError:
-                print("Ocurrio un problema al crear el archivo '.config'")
-
-        ip = input("Ingresa el ultimo campo de la direccion IP: ")
-        ipa.first_configuration(ip)
-
-        while True:
-            new_change = input('Quieres cambiar la dirección de nuevo? (Y/N)').upper()
-            if new_change == 'Y':        
-                ip = input("Ingresa la nueva direccion: ")
-                ipa.new_config(ip)
-
-            elif new_change == 'N':
-                break
-            else:
-                print('Entrada no valida. Por favor, ingresa "Y" para si o "N" para no.')
-
-    if PRODUCTION_MODE == False:
-        # Revision del diccionario:
-        # La clave IP (para este caso) debe ser en este formato 192.168.0.
-        for key in ipa.dictionary:
-            print(key, ipa.dictionary[f'{key}'])
+    # def set_dns():
+    #     command_DNS1 = ['netsh', 'interface', 'ipv4', 'set', 'dnsservers', f'{base_config['NAME']}', 'static', f'{base_config['DNS1']}', 'primary']
+    #     print(f"Servidor DNS1: {base_config['DNS1']}")
+    #     run(command_DNS1)
+    
+    # def add_dns():
+    #     command_DNS2 = ['netsh', 'interface', 'ipv4', 'add', 'dnsservers', f'{base_config['NAME']}', f'{base_config['DNS2']}']
+    #     print(f"Servidor DNS2: {base_config['DNS2']}")
+    #     run(command_DNS2)
